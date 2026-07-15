65c8763118e0b7c99947117873f77e079e1bf770
goakiller900 <remco.kappers@gmail.com>
2026-07-15 23:14:07 +0200

    Add Railwright runtime constants

---
+++ scripts/constants.lua
@@ -0,0 +1,36 @@
+local Constants = {
+    mod_name = "railwright",
+
+    gui = {
+        top_button = "railwright_top_button",
+        frame = "railwright_main_frame",
+        titlebar = "railwright_titlebar",
+        close_button = "railwright_close_button",
+        content = "railwright_content",
+        generate_button = "railwright_generate_button",
+
+        station_type = "railwright_station_type",
+        station_name = "railwright_station_name",
+        locomotives = "railwright_locomotives",
+        cargo_wagons = "railwright_cargo_wagons",
+        double_headed = "railwright_double_headed",
+        include_train = "railwright_include_train",
+        sides = "railwright_sides",
+        inserter = "railwright_inserter",
+        chest = "railwright_chest",
+        belt = "railwright_belt",
+    },
+
+    station_types = {
+        "Loading station",
+        "Unloading station",
+    },
+
+    sides = {
+        "Both",
+        "Right",
+        "Left",
+    },
+}
+
+return Constants
